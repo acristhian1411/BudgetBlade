0fb41d1624ecd566660cab5701c45939c9a514cb
acristhian1411 <acristhian35@gmail.com>
2024-02-27 20:32:12 -0300

    added styles to the layout

--- svelteUI/src/routes/+page.svelte
+++ svelteUI/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Bienvenido a BudgetBlade</h1>
+<p>Aca se mostraran gráficos</p>
