9ec590c49a5c6d574452c926d3dc11276a7258e0
acristhian1411 <acristhian35@gmail.com>
2024-03-09 08:52:01 -0300

    Add error handling and display, add show button to table

--- svelteUI/src/routes/tilltypes/+page.svelte
+++ svelteUI/src/routes/tilltypes/+page.svelte
@@ -118,7 +118,28 @@
 </script>
 
 {#if error}
-	<p>{error}</p>
+	<div
+		class="relative rounded border border-red-400 bg-red-100 px-4 py-2 text-red-700"
+		role="alert"
+	>
+		<strong class="font-bold">Error:</strong>
+		<span class="block sm:inline">{error}</span>
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<span class="absolute bottom-0 right-0 top-0 px-4 py-3" on:click={() => (error = null)}>
+			<svg
+				class="h-6 w-6 fill-current text-red-500"
+				role="button"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+			>
+				<title>Close</title>
+				<path
+					d="M14.348 14.849c-.469.469-1.229.469-1.697 0L10 11.819l-2.651 3.029c-.469.469-1.228.469-1.697 0-.469-.469-.469-1.228 0-1.697l2.756-3.15L5.652 5.151c-.469-.469-.469-1.229 0-1.697.469-.469 1.228-.469 1.697 0l3.65 4.17 3.65-4.17c.469-.469 1.228-.469 1.697 0 .469.469.469 1.228 0 1.697l-2.756 3.15 2.756 3.15c.469.469.469 1.228 0 1.697z"
+				/>
+			</svg>
+		</span>
+	</div>
 {/if}
 <h3 class="mb-4 text-center text-2xl">Tipos de Cajas</h3>
 
@@ -213,6 +234,9 @@
 						<td>{i + 1}</td>
 						<td>{t_type.id}</td>
 						<td class="text-center">{t_type.t_type_desc}</td>
+						<td>
+							<a href="/tilltypes/{t_type.id}" class="btn btn-info">Mostrar</a>
+						</td>
 						<td>
 							<button class="btn btn-warning" on:click={() => openEditModal(t_type)}>Editar</button>
 						</td>
---
+++ svelteUI/src/routes/tilltypes/[id]/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	let error;
+	let tilltype;
+
+	export let data;
+
+	async function fetchData() {
+		axios
+			.get(`http://127.0.0.1:3000/api/tillstypes/${data.id}`)
+			.then((response) => {
+				tilltype = response.data;
+			})
+			.catch((err) => {
+				error = err;
+			});
+		// })
+	}
+
+	onMount(() => {
+		fetchData();
+	});
+</script>
+
+<!-- <h1>{data.id}</h1> -->
+{#if error}
+	<p>{error}</p>
+{/if}
+<div class="breadcrumbs text-sm">
+	<ul>
+		<li><a href="/">Inicio</a></li>
+		<li><a href="/tilltypes">Tipos de caja</a></li>
+	</ul>
+</div>
+{#if tilltype}
+	<h1 class="text-2xl font-bold">Descripcion:</h1>
+	<p class="text-1xl">{tilltype.t_type_desc}</p>
+{/if}
+<!-- <div>{@html data.content}</div> -->
