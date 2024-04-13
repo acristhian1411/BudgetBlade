2909111e49dc079fa2a4052150e0c903642b513e
acristhian1411 <acristhian35@gmail.com>
2024-04-13 13:01:03 -0400

    add componets for persontypes

---
+++ svelteUI/src/routes/persontypes/[id]/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	let error;
+	let tilltype;
+
+	export let data;
+
+	async function fetchData() {
+		axios
+			.get(`http://127.0.0.1:3000/api/persontypes/${data.id}`)
+			.then((response) => {
+				tilltype = response.data;
+			})
+			.catch((err) => {
+				error = err;
+			});
+		// })
+	}
+
+	onMount(() => {
+		fetchData();
+	});
+</script>
+
+<!-- <h1>{data.id}</h1> -->
+{#if error}
+	<p>{error}</p>
+{/if}
+<div class="breadcrumbs text-md mb-4">
+	<ul>
+		<li><a href="/">Inicio</a></li>
+		<li><a href="/persontypes">Tipos de personas</a></li>
+	</ul>
+</div>
+{#if tilltype}
+	<h1 class="text-xl font-bold">Descripcion:</h1>
+	<p class="text-1xl">{tilltype.p_type_desc}</p>
+{/if}
+<!-- <div>{@html data.content}</div> -->
---
+++ svelteUI/src/routes/persontypes/form.svelte
@@ -0,0 +1,76 @@
+<script>
+	// @ts-nocheck
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	let id = 0;
+	let p_type_desc = '';
+	export let edit;
+	export let item;
+	function close() {
+		dispatch('close');
+	}
+
+	function OpenAlertMessage(event) {
+		dispatch('message', event.detail);
+	}
+
+	onMount(() => {
+		if (edit == true) {
+			id = item.id;
+			p_type_desc = item.p_type_desc;
+		}
+	});
+	// http://127.0.0.1:5173/tilltypes
+	function handleCreateObject() {
+		axios
+			.post(`http://127.0.0.1:3000/api/persontypes`, {
+				p_type_desc
+			})
+			.then((res) => {
+				let detail = {
+					detail: {
+						type: 'success',
+						message: res.data.message
+					}
+				};
+				OpenAlertMessage(detail);
+				close();
+			});
+	}
+	function handleUpdateObject() {
+		axios
+			.put(`http://127.0.0.1:3000/api/persontypes/${id}`, {
+				p_type_desc
+			})
+			.then((res) => {
+				let detail = {
+					detail: {
+						type: 'success',
+						message: res.data.message
+					}
+				};
+				OpenAlertMessage(detail);
+				close();
+			});
+	}
+</script>
+
+{#if edit == true}
+	<h3 class="mb-4 text-center text-2xl">Actualizar Tipo de Persona</h3>
+{:else}
+	<h3 class="mb-4 text-center text-2xl">Crear Tipo de Persona</h3>
+{/if}
+<form>
+	<div class="mb-4 flex items-center">
+		<span class="mr-2">Descripción</span>
+		<input type="text" bind:value={p_type_desc} class="input input-bordered w-full max-w-xs" />
+	</div>
+
+	<button
+		class="btn btn-primary"
+		on:click={edit == true ? handleUpdateObject() : handleCreateObject()}>Guardar</button
+	>
+	<button class="btn btn-secondary" on:click={close}>Cancelar</button>
+</form>
