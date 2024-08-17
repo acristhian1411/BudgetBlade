2d5b4f9f80b8b4614b357df5314345fd978626b3
acristhian1411 <acristhian35@gmail.com>
2024-08-17 11:08:05 -0400

    add env variable to login

--- svelteUI/src/services/authservice.js
+++ svelteUI/src/services/authservice.js
@@ -1,10 +1,11 @@
 import { goto } from "$app/navigation";
 // import  jsonwebtoken from 'jsonwebtoken';
 import * as jose from 'jose';
+import { PUBLIC_APP_URL } from '$env/static/public';
 // import { PUBLIC_JWT_SECRET } from '$env/static/public';
 
 export async function login(email, password) {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
+    const response = await fetch(`${PUBLIC_APP_URL}/api/auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
