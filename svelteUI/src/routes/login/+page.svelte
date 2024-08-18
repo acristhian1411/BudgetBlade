8c6a0ce3ba019123c2b62f1446cb4ccbb35e31bf
acristhian1411 <acristhian35@gmail.com>
2024-08-17 21:08:08 -0400

    add styles to login and register

--- svelteUI/src/routes/login/+page.svelte
+++ svelteUI/src/routes/login/+page.svelte
@@ -30,47 +30,41 @@
 		
 	}
 </script>
-<style>
-	.password-container {
-	  position: relative;
-	  display: flex;
-	  align-items: center;
-	}
-  
-	.password-input {
-	  padding-right: 40px;
-	  width: 100%;
-	}
-  
-	.toggle-button {
-	  position: absolute;
-	  right: 10px;
-	  background: none;
-	  border: none;
-	  cursor: pointer;
-	}
-  </style>
-<form>
-	<input type="email" bind:value={email} placeholder="email" />
-	<div class="password-container">
-		{#if showPassword}
-			<input
-			class="password-input"
-			type="text"
-			bind:value={password}
-			placeholder="Password"
-			/>
-		{:else}
-			<input
-			class="password-input"
-			type="password"
-			bind:value={password}
-			placeholder="Password"
-			/>
-		{/if}
-		<button type="button" class="toggle-button" on:click={togglePasswordVisibility}>
-		  {showPassword ? '🙈' : '👁️'}
-		</button>
-	  </div>
-	<button type="submit" on:click={() => handleLogin(email, password)}>Login</button>
-</form>
+
+
+
+<section >
+	<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+		
+		<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+			<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+				<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+					Sign in to your account
+				</h1>
+				<form class="space-y-4 md:space-y-6" action="#">
+					<div>
+						<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+						<input type="email" bind:value={email} name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
+					</div>
+					<div>
+						<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+						<input type="password" bind:value={password} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+					</div>
+					<div class="flex items-center justify-between">
+						<div class="flex items-start">
+							<div class="flex items-center h-5">
+								<input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required>
+							</div>
+							
+						</div>
+						
+					</div>
+					<button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" on:click={() => handleLogin(email, password)}>Sign in</button>
+					<p class="text-sm font-light text-gray-500 dark:text-gray-400">
+						Don’t have an account yet? <a href="/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+					</p>
+				</form>
+			</div>
+		</div>
+	</div>
+  </section>
